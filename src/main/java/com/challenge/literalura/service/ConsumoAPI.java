7f3dc73c23361c9ee9408d1f4a6ea9b3aaa14afe
gabriel-gett <gett.912@gmail.com>
2025-07-17 13:52:44 -0600

    Construyendo solicitud API

---
+++ src/main/java/com/challenge/literalura/service/ConsumoAPI.java
@@ -0,0 +1,30 @@
+package com.challenge.literalura.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+
+    public String obtenerDatos(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException("Error al obtener datos de la API", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("la solicitud fue interrumpida", e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
