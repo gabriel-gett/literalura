82cdb13baa01e05ea16923b66a501a119d5a312a
gabriel-gett <gett.912@gmail.com>
2025-07-23 16:10:11 -0600

    analizando respuesta json

---
+++ src/main/java/com/challenge/literalura/service/ConvierteDatos.java
@@ -0,0 +1,17 @@
+package com.challenge.literalura.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvierteDatos implements IConvierteDatos {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T getData(String json, Class<T> tClass) {
+        try {
+            return objectMapper.readValue(json, tClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
